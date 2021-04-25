f10e41020e5caf980a1c3c5b52a71aafb2077f14
DuncantheeDuncan <mthembup69@gmail.com>
2021-04-25 02:44:18 +0200

    added level three

--- src/main/java/challenge/coding/PrintTree.java
+++ src/main/java/challenge/coding/PrintTree.java
@@ -1,5 +1,30 @@
 package challenge.coding;
 
 public class PrintTree {
+	public static void main(String[] args) {
+		
+		PrintTree i = new PrintTree();
+
+		i.levelThreeTree();
+	}
+
+
+
+
+	public void levelThreeTree() { 
+
+		for (int i = 0; i < 6; i++) {
+
+			for (int k = 0; k < 5 - i; k++)
+				System.out.print(" ");
+			
+			for (int j = 0; j <= i; j++)
+				System.out.print("* ");
+			
+			System.out.println();
+		} 
+	}
+
+
 
 }
--- src/main/java/run.sh
+++ src/main/java/run.sh
@@ -1,9 +1,9 @@
 
+javac challenge/coding/PrintTree.java
+
+java challenge.coding.PrintTree
+
+
 # javac challenge/coding/FibonacciSequence.java 
 
 # java challenge.coding.FibonacciSequence 
-
-
-javac challenge/coding/FibonacciSequence.java 
-
-java challenge.coding.FibonacciSequence 
